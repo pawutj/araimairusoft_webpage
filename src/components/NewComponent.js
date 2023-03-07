a594b21670aac80a6a71a097bac5efefb6a56c1a
astralaria <pawut.j@gmail.com>
2023-03-07 21:40:01 +0700

    Add

---
+++ src/components/NewComponent.js
@@ -0,0 +1,3 @@
+export default function NewComponent({ text = "" }) {
+  return <div className="new-component">{text}</div>;
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
