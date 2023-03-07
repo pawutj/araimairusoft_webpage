f5d141e5168f9fcd14fb9ae792312c65d9fd1980
Kittipat <first1801@hotmail.com>
2023-03-07 22:23:17 +0700

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return <div className="navbar navbar-bg mynavbar">
+        <h1 className="navbar-brand mynavbar-brand">aRaimaiRuSoft</h1>
+        <nav>
+            <ul className="nav-menu">
+                <li className="navbar-ul-li-item">News</li>
+                <li className="navbar-ul-li-item">Article</li>
+                <li className="navbar-ul-li-item">Product</li>
+                <li className="navbar-ul-li-item">Staff</li>
+                <li className="navbar-ul-li-item">Contact</li>
+            </ul>
+        </nav>
+    </div>;
+  }
+  
