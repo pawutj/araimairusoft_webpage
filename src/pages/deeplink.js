110e0a949fe71711d99ed1077c0e1cf58409f152
pawut jingjit <pawut.j@gmail.com>
2023-05-18 20:49:47 +0700

    git add Deeplink

---
+++ src/pages/deeplink.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Deeplink = () => {
+  return(
+    <div>
+  <div style={{BackGroundColor : "#FFFFFFF"}}>
+    <a href='http://com.defaultcompany.testdeeplink/your-path'>
+        <button >Just Button</button>
+    </a>
+    </div>
+    </div>
+  )
+
+};
+
+export default Deeplink;
